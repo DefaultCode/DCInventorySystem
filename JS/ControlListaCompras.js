b8ff9291b42b66cc0543384c274dc72f3aa24c17
AliMarcano <ali-marcano_10@hotmail.com>
2020-05-22 18:43:24 -0400

    commit 24

--- JS/ControlListaCompras.js
+++ JS/ControlListaCompras.js
@@ -56,7 +56,9 @@ function calcularMonto(tables,i,C1, C2) {
 }
 
 function newProveedor(){
-	if (document.getElementById("datosProve").style.display == "none") {
+	window.open("../HTML/AgregarProveedor.html?","New Proveedor","width=720,height=400,top=85,left=350");
+
+	/*if (document.getElementById("datosProve").style.display == "none") {
 		document.getElementById("IDProveedor").value = "";
 		document.getElementById("NombreProveedor").value = "";
 		document.getElementById("DireccionProveedor").value = "";
@@ -73,7 +75,7 @@ function newProveedor(){
 		document.getElementById("producto").style.marginTop = "70px";
 
 	}
-
+*/
 }
 
 function addHTMLTableRow(){
